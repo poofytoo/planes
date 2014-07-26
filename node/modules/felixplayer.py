0433bb7366f9fd857ce3884166d855bd19751061
Felix Sun <fephsun@gmail.com>
2014-07-25 22:50:53 -0400

    Felix's player

---
+++ node/modules/felixplayer.py
@@ -0,0 +1,29 @@
+import json
+import random
+
+# YOUR CODE GOES HERE
+# returns one of 'shoot', 'charge', 'up', 'down'
+def compute(gameRound, enemyRow, myRow, radars, myAmmo, myLastMove):
+  candidates = {'shoot': 1, 'charge': 1, 'up': 1, 'down': 1}
+  if radars[myRow] == 'ALERT':
+    candidates['shoot'] = 0
+    candidates['charge'] = 0
+  if myAmmo == 0:
+    candidates['shoot'] = 0
+  if myRow == 0:
+    candidates['up'] = 0
+  if myRow == 4:
+    candidates['down'] = 0
+  possible = [move for move in candidates if candidates[move] == 1]
+  return random.sample(possible, 1)[0]
+
+def wrapper():
+  data = raw_input()
+  js = json.loads(data)
+  radars = []
+  for s in range(5):
+    radars.append(js['radar' + str(s)])
+  output = compute(js['gameRound'], js['enemyRow'], js['myRow'], radars, js['ammo'], js['lastMove'])
+  print output
+
+wrapper()
