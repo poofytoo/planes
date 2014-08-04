2393114b66f558eb7825bbf368937cd690db3f4a
poofytoo <victorhung92@gmail.com>
2014-08-04 01:33:43 -0400

    end screens

--- gameserver/modules/example-bot.py
+++ gameserver/modules/example-bot.py
@@ -1,3 +1,5 @@
+import json
+
 def compute(gameRound, enemyRow, myRow, radars, myAmmo, myLastMove):
   # This bot charges, shoots, then moves down
   if gameRound % 3 == 0:
@@ -5,4 +7,17 @@ def compute(gameRound, enemyRow, myRow, radars, myAmmo, myLastMove):
   elif gameRound % 3 == 1:
     return 'shoot'
   else:
-    return 'down'
+    return 'charge'
+
+# You should not edit anything below this line
+
+def wrapper():
+  data = raw_input()
+  js = json.loads(data)
+  radars = []
+  for s in range(5):
+    radars.append(js['radar' + str(s)])
+  output = compute(js['gameRound'], js['enemyRow'], js['myRow'], radars, js['ammo'], js['lastMove'])
+  print output
+
+wrapper()
